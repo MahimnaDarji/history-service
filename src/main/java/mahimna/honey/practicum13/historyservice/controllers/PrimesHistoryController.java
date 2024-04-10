118f49a87e512f71d064328283dbf44cc812949d
Darji, Mahimna Gaurang <mdarji@iu.edu>
2024-04-09 21:39:53 -0400

    Practicum 13 Changes

--- src/main/java/mahimna/honey/practicum13/historyservice/controllers/PrimesHistoryController.java
+++ src/main/java/mahimna/honey/practicum13/historyservice/controllers/PrimesHistoryController.java
@@ -1,4 +1,20 @@
 package mahimna.honey.practicum13.historyservice.controllers;
 
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
 public class PrimesHistoryController {
+    
+    private final PrimesHistoryRepository primesHistoryRepository;
+    
+    public PrimesHistoryController(
+        PrimesHistoryRepository primesHistoryRepository) { 
+        this.primesHistoryRepository = primesHistoryRepository;
+}
+
+@GetMapping("/{customer}")
+public List<Primes Record> findAll(@PathVariable String customer) { 
+    return primesHistoryRepository.findAllByCustomer (customer);
+}
 }
--- src/main/java/mahimna/honey/practicum13/historyservice/rabbitmq/MQReceiver.java
+++ src/main/java/mahimna/honey/practicum13/historyservice/rabbitmq/MQReceiver.java
@@ -1,4 +1,22 @@
 package mahimna.honey.practicum13.historyservice.rabbitmq;
 
+
+@Component
 public class MQReceiver {
+    
+    private final PrimesHistoryRepository primesHistoryRepository;
+
+    public MQReceiver (PrimesHistoryRepository primesHistoryRepository) { 
+        this.primesHistoryRepository = primesHistoryRepository;
+}
+
+@RabbitListener(queues = {"primes"})
+
+public void receiveMessage (@Payload String message) {
+    System.out.println(message);
+    Gson gson = new Gson();
+    PrimesRecord primesRecord = gson
+        .fromJson (message, PrimesRecord.class);
+primesHistoryRepository.save(primesRecord);
+}
 }
